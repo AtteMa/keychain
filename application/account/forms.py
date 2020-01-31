558a5eb1080cb115bb605b05c26a10bbef42832f
AtteMa <martikainen.atte@gmail.com>
2020-01-31 12:12:02 +0200

    added user creation

--- application/account/forms.py
+++ application/account/forms.py
@@ -1,9 +1,19 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField
+from wtforms import PasswordField, StringField, validators
 
 class LoginForm(FlaskForm):
     username = StringField("Username")
     password = PasswordField("Password")
+
+    class Meta:
+        csrf=False
+
+class AccountForm(FlaskForm):
+    name = StringField("Name", [validators.Length(min=1)])
+    username = StringField("Username", [validators.Length(min=1)])
+    password = PasswordField("Password", [validators.InputRequired(),
+                validators.EqualTo('confirm', message='Passwords must match')])
+    confirm = PasswordField("Repeat password")
 
     class Meta:
         csrf=False
--- application/account/views.py
+++ application/account/views.py
@@ -3,7 +3,7 @@ from flask_login import login_user, logout_user
 
 from application import app, db
 from application.account.models import Account
-from application.account.forms import LoginForm
+from application.account.forms import LoginForm, AccountForm
 
 @app.route("/account/login", methods = ["GET", "POST"])
 def account_login():
@@ -24,4 +24,22 @@ def account_login():
 @app.route("/account/logout")
 def account_logout():
     logout_user()
+    return redirect(url_for("index"))
+
+@app.route("/account/new/")
+def account_form():
+    return render_template("account/new.html", form = AccountForm())
+
+@app.route("/account/", methods=["POST"])
+def account_create():
+    form = AccountForm(request.form)
+
+    if not form.validate():
+        return render_template("account/new.html", form = form)
+
+    p = Account(form.name.data, form.username.data, form.password.data)
+    
+    db.session().add(p)
+    db.session().commit()
+
     return redirect(url_for("index"))
