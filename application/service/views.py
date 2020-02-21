1dcc8312fd8481c0c99ae38ecf810a5cc29853e5
AtteMa <martikainen.atte@gmail.com>
2020-02-21 20:49:00 +0200

    deleted unnecessary files

--- application/service/views.py
+++
@@ -1,5 +0,0 @@
-from application import app, db
-from flask import render_template, request, redirect, url_for
-from application.service.models import Service
-from application.passwords.forms import PasswordForm, UpdateForm
-
